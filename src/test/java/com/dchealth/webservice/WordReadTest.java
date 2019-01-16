b18cbec7e4b1a77326ee4453f9c646993200c4eb
sf201 <shuaifei201@163.com>
2019-01-16 17:21:44 +0800

    1、添加生成pb对象的方法。

--- src/test/java/com/dchealth/webservice/WordReadTest.java
+++ src/test/java/com/dchealth/webservice/WordReadTest.java
@@ -1,6 +1,8 @@
 package com.dchealth.webservice;
 
 
+import com.dchealth.webservice.utils.ClassScaner;
+import com.dchealth.webservice.utils.StringUtil;
 import com.dchealth.webservice.vo.BussinessCode;
 import org.apache.poi.ooxml.POIXMLDocumentPart;
 import org.apache.poi.openxml4j.exceptions.OpenXML4JException;
@@ -10,13 +12,15 @@ import org.dom4j.DocumentException;
 import org.dom4j.Element;
 import org.dom4j.io.SAXReader;
 import org.junit.Test;
+import org.springframework.beans.BeanUtils;
+import org.springframework.context.annotation.Bean;
+import org.springframework.util.ClassUtils;
+import org.springframework.util.StringUtils;
 
+import java.beans.PropertyDescriptor;
 import java.io.*;
 import java.lang.reflect.Field;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 
 public class WordReadTest {
 
@@ -26,7 +30,7 @@ public class WordReadTest {
         HashMap<String, String> nameKeys = this.getNameKeys();
 
 //        POIXMLDocument
-        FileInputStream fileInputStream = new FileInputStream(new File("C:\\Users\\Administrator\\IdeaProjects\\web-service\\src\\test\\java\\com\\dchealth\\webservice\\1.docx"));
+        FileInputStream fileInputStream = new FileInputStream(new File(this.getClass().getResource("/1.docx").getPath()));
         XWPFDocument document = new XWPFDocument(fileInputStream);
 
         System.out.println("----------------------------所有文档结束----------------------------------");
@@ -152,12 +156,16 @@ public class WordReadTest {
 
     @Test
     public void createClass() throws OpenXML4JException, IllegalAccessException, IOException, DocumentException {
-        List<ObjectSupper> objectSuppers = this.readWordTest();
-        objectSuppers = rebuildObjectSuppers(objectSuppers);
+
+
+//        List<ObjectSupper> objectSuppers = this.readWordTest();
+//        objectSuppers = rebuildObjectSuppers(objectSuppers);
 //        objectSuppers = nextBuildObjectSuppers(objectSuppers);//去重
 //        createMessage(objectSuppers);
-        createResponse(objectSuppers);
-        createService(objectSuppers);
+//        createPbMessage(objectSuppers);
+        createPbResponse();
+//        createResponse(objectSuppers);
+//        createService(objectSuppers);
 
     }
 
@@ -170,32 +178,32 @@ public class WordReadTest {
             String responseName = objectSupper.getResponseName();
             System.out.println(responseName);
             String className = this.getClassName(objectSupper) + "Response";
-            String str = "import javax.xml.bind.annotation.XmlRootElement;\n" +
-                    "\n" +
-                    "@XmlRootElement(name = \"${response}\")\n";
+            String str = "import javax.xml.bind.annotation.XmlRootElement;\r\n" +
+                    "\r\n" +
+                    "@XmlRootElement(name = \"${response}\")\r\n";
             String response = "";
 
 
             StringBuilder stringBuilder = new StringBuilder();
-            stringBuilder.append("public class "+className+"{\n");
+            stringBuilder.append("public class " + className + "{\r\n");
             List<FieldBean> responseBean = objectSupper.getResponseBean();
-            for(FieldBean bean :responseBean){
+            for (FieldBean bean : responseBean) {
                 if (responseName != null) {
                     response = responseName;
-                    if("result".equals(bean.getName())||"desc".equals(bean.getName())||"resultCode".equals(bean.getName())){
+                    if ("result".equals(bean.getName()) || "desc".equals(bean.getName()) || "resultCode".equals(bean.getName())) {
                         continue;
                     }
-                }else{
+                } else {
                     response = "response";
                 }
-                stringBuilder.append("\tprivate "+bean.getfType()+" "+bean.getName()+";//"+bean.getCnName()+"|"+bean.getDesc()+"\n");
+                stringBuilder.append("\tprivate " + bean.getfType() + " " + bean.getName() + ";//" + bean.getCnName() + "|" + bean.getDesc() + "\r\n");
 
             }
-            stringBuilder.append("}") ;
-            str = str.replace("${response}",response);
-            str+=stringBuilder.toString();
+            stringBuilder.append("}");
+            str = str.replace("${response}", response);
+            str += stringBuilder.toString();
 
-            File file = new File(className+".java");
+            File file = new File(this.getClass().getResource("").getPath() + "/" + className + ".java");
             if (!file.exists()) {
                 file.createNewFile();
             }
@@ -209,7 +217,7 @@ public class WordReadTest {
 
     private void createMessage(List<ObjectSupper> objectSuppers) throws IOException {
 
-        File rFile = new File("C:\\Users\\Administrator\\IdeaProjects\\web-service\\src\\test\\java\\com\\dchealth\\webservice\\request.template");
+        File rFile = new File(this.getClass().getResource("/PbRequest.template").getPath());
         FileReader fileReader = new FileReader(rFile);
         char[] buffer = new char[2000];
         fileReader.read(buffer);
@@ -224,9 +232,9 @@ public class WordReadTest {
             List<FieldBean> messageBean = objectSupper.getMessageBean();
             for (FieldBean bean : messageBean) {
                 String fileStr = "\tprivate " + bean.getfType() + " " + bean.getName() + " ;//" + bean.getCnName() + "|" + bean.getDesc();
-                template += fileStr + "\n";
+                template += fileStr + "\r\n";
             }
-            template += "\tpublic " + className + "(){\n\t}\n";
+            template += "\tpublic " + className + "(){\r\n\t}\r\n";
             template += "\t";
             template += "}";
             File file = new File(className + ".java");
@@ -240,6 +248,96 @@ public class WordReadTest {
         }
     }
 
+    private void createPbMessage(List<ObjectSupper> objectSuppers) throws IOException {
+
+        File rFile = new File(this.getClass().getResource("/PbRequest.template").getPath());
+        FileReader fileReader = new FileReader(rFile);
+        char[] buffer = new char[2000];
+        fileReader.read(buffer);
+        String souceTemplate = new String(buffer);
+        fileReader.close();
+
+        for (ObjectSupper objectSupper : objectSuppers) {
+
+            String className = this.getPbClassName(objectSupper) + "_message";
+            String template = souceTemplate;
+            template = template.replace("${className}", className).trim();
+            List<FieldBean> messageBean = objectSupper.getMessageBean();
+            StringBuilder sbVarible = new StringBuilder();
+            StringBuilder sbToJson = new StringBuilder();
+            StringBuilder sbParseJson = new StringBuilder();
+
+            for (FieldBean bean : messageBean) {
+                sbVarible.append(bean.getfType()).append(" msg_").append(bean.getName()).append("\r\n");
+                sbToJson.append("json.setattribute( \"").append(bean.getName()).append("\",").append("msg_").append(bean.getName()).append(")").append("\r\n");
+                sbParseJson.append("msg_").append(bean.getName()).append("= json.getattribute( \"").append(bean.getName()).append("\")").append("\r\n");
+            }
+            template = template.replace("${variables}", sbVarible.toString());
+            template = template.replace("${toJson}", sbToJson.toString());
+            template = template.replace("${parseJson}", sbParseJson.toString());
+            File file = new File(this.getClass().getResource("").getPath() + "/" + className + ".sru");
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+            FileWriter fileWriter = new FileWriter(file);
+            fileWriter.write(template);
+            fileWriter.flush();
+            fileWriter.close();
+        }
+    }
+
+    private String readTemplate(String path) throws IOException {
+        File rFile = new File(path);
+        FileReader fileReader = new FileReader(rFile);
+        char[] buffer = new char[2000];
+        fileReader.read(buffer);
+        String souceTemplate = new String(buffer);
+        return souceTemplate;
+    }
+    private void createPbResponse() throws IOException {
+
+        String souceTemplate = this.readTemplate(this.getClass().getResource("/PbResponse.template").getPath());
+        String parseJsonEntitiesTemplate = this.readTemplate(this.getClass().getResource("/PbResponseParseJsonEntities.template").getPath());
+        String toJsonEntitiesTemplate = this.readTemplate(this.getClass().getResource("/PbResponseToJsonEntities.template").getPath());
+        Set<Class> set = ClassScaner.scan("com.dchealth.webservice.vo.response", null);
+        String entityName = "person_info";
+        for (Class clazz : set) {
+            String template = souceTemplate;
+            StringBuilder sbVarible = new StringBuilder();
+            StringBuilder sbToJson = new StringBuilder();
+            StringBuilder sbParseJson = new StringBuilder();
+            String className = StringUtil.underscoreName(clazz.getSimpleName());
+            parseJsonEntitiesTemplate = parseJsonEntitiesTemplate.replace("${entityName}", entityName);
+            toJsonEntitiesTemplate = toJsonEntitiesTemplate.replace("${entityName}", entityName);
+            for (PropertyDescriptor d : BeanUtils.getPropertyDescriptors(clazz)) {
+                if (d.getName().equalsIgnoreCase("class")) {
+                    continue;
+                }
+                if (d.getName().equalsIgnoreCase("entities")) {
+                    sbVarible.append(entityName).append(" ").append("res_entities[]").append("\r\n");
+                    sbParseJson.append(parseJsonEntitiesTemplate).append("\r\n");
+                    sbToJson.append(toJsonEntitiesTemplate).append("\r\n");
+                } else {
+                    sbVarible.append(d.getPropertyType().getSimpleName()).append(" ").append("res_").append(d.getName()).append("\r\n");
+                    sbToJson.append("json.setattribute( \"").append(d.getName()).append("\",").append("res_").append(d.getName()).append(")").append("\r\n");
+                    sbParseJson.append("res_").append(d.getName()).append("= json.getattribute( \"").append(d.getName()).append("\")").append("\r\n");
+                }
+
+            }
+            template = template.replace("${className}", className).trim();
+            template = template.replace("${variables}", sbVarible.toString());
+            template = template.replace("${toJson}", sbToJson.toString());
+            template = template.replace("${parseJson}", sbParseJson.toString());
+            File file = new File(this.getClass().getResource("").getPath() + "/" + className + ".sru");
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+            FileWriter fileWriter = new FileWriter(file);
+            fileWriter.write(template);
+            fileWriter.flush();
+            fileWriter.close();
+        }
+    }
 
     private String getClassName(ObjectSupper objectSupper) {
         String name = objectSupper.getName();
@@ -252,6 +350,11 @@ public class WordReadTest {
             stringBuilder.append(msg);
         }
         return stringBuilder.toString();
+    }
+
+    private String getPbClassName(ObjectSupper objectSupper) {
+        String name = objectSupper.getName();
+        return name.toLowerCase();
     }
 
     /**
